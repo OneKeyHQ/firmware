d727997ec4e1b7fdfbcf0d9b65d144d85bf503a9
dahuanzi <lihuanza@163.com>
2020-05-18 12:36:50 +0800

    delete trezor signature public key

--- legacy/bootloader/signatures.c
+++ legacy/bootloader/signatures.c
@@ -33,14 +33,6 @@ const uint32_t FIRMWARE_MAGIC_BLE = 0x33383235;  // 5283
 
 #define PUBKEYS 5
 
-static const uint8_t * const pubkey[PUBKEYS] = {
-	(const uint8_t *)"\x04\xd5\x71\xb7\xf1\x48\xc5\xe4\x23\x2c\x38\x14\xf7\x77\xd8\xfa\xea\xf1\xa8\x42\x16\xc7\x8d\x56\x9b\x71\x04\x1f\xfc\x76\x8a\x5b\x2d\x81\x0f\xc3\xbb\x13\x4d\xd0\x26\xb5\x7e\x65\x00\x52\x75\xae\xde\xf4\x3e\x15\x5f\x48\xfc\x11\xa3\x2e\xc7\x90\xa9\x33\x12\xbd\x58",
-	(const uint8_t *)"\x04\x63\x27\x9c\x0c\x08\x66\xe5\x0c\x05\xc7\x99\xd3\x2b\xd6\xba\xb0\x18\x8b\x6d\xe0\x65\x36\xd1\x10\x9d\x2e\xd9\xce\x76\xcb\x33\x5c\x49\x0e\x55\xae\xe1\x0c\xc9\x01\x21\x51\x32\xe8\x53\x09\x7d\x54\x32\xed\xa0\x6b\x79\x20\x73\xbd\x77\x40\xc9\x4c\xe4\x51\x6c\xb1",
-	(const uint8_t *)"\x04\x43\xae\xdb\xb6\xf7\xe7\x1c\x56\x3f\x8e\xd2\xef\x64\xec\x99\x81\x48\x25\x19\xe7\xef\x4f\x4a\xa9\x8b\x27\x85\x4e\x8c\x49\x12\x6d\x49\x56\xd3\x00\xab\x45\xfd\xc3\x4c\xd2\x6b\xc8\x71\x0d\xe0\xa3\x1d\xbd\xf6\xde\x74\x35\xfd\x0b\x49\x2b\xe7\x0a\xc7\x5f\xde\x58",
-	(const uint8_t *)"\x04\x87\x7c\x39\xfd\x7c\x62\x23\x7e\x03\x82\x35\xe9\xc0\x75\xda\xb2\x61\x63\x0f\x78\xee\xb8\xed\xb9\x24\x87\x15\x9f\xff\xed\xfd\xf6\x04\x6c\x6f\x8b\x88\x1f\xa4\x07\xc4\xa4\xce\x6c\x28\xde\x0b\x19\xc1\xf4\xe2\x9f\x1f\xcb\xc5\xa5\x8f\xfd\x14\x32\xa3\xe0\x93\x8a",
-	(const uint8_t *)"\x04\x73\x84\xc5\x1a\xe8\x1a\xdd\x0a\x52\x3a\xdb\xb1\x86\xc9\x1b\x90\x6f\xfb\x64\xc2\xc7\x65\x80\x2b\xf2\x6d\xbd\x13\xbd\xf1\x2c\x31\x9e\x80\xc2\x21\x3a\x13\x6c\x8e\xe0\x3d\x78\x74\xfd\x22\xb7\x0d\x68\xe7\xde\xe4\x69\xde\xcf\xbb\xb5\x10\xee\x9a\x46\x0c\xda\x45",
-};
-
 static const uint8_t * const pubkey_Bixin[PUBKEYS] = {
 	(const uint8_t *)"\x04\xad\x90\x35\xd6\x7a\xc4\x79\x5c\x91\x3c\x45\x2d\x25\x15\x6f\x0b\x09\x4c\x34\xf6\x56\xa2\x49\xb9\x4d\x8d\x66\x19\xab\x0d\x92\xb1\xe8\xbc\xc3\x28\xbd\xc8\x33\xb9\xb5\x1c\xa3\x1b\xfd\x01\x36\x61\x51\x53\xf9\x3a\xba\x46\xd0\x2a\xb5\xd9\x25\xf4\xf3\x64\xc6\x66",
 	(const uint8_t *)"\x04\x95\xb8\x3d\xa4\x42\xc6\x89\xbd\xa8\x2e\x9f\x95\x43\x81\x1f\xec\x2f\x58\x33\x4f\x5c\x76\x36\x1f\x5a\x49\xfb\xb6\x63\x4a\x81\x15\x2c\x6d\xa7\xb8\xa1\x78\x2c\xca\xa9\x28\x7e\xc7\xa8\xef\xe9\xd8\xbb\xa7\xd9\x01\x80\xf7\xb7\x19\xc0\x17\xd8\x04\xd6\x1c\x3d\x5d",
@@ -102,29 +94,17 @@ int signatures_old_ok(void) {
   if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[sigindex1 - 1],
                                (const uint8_t *)FLASH_META_SIG1,
                                hash)) {  // failure
-    if (0 != ecdsa_verify_digest(&secp256k1, pubkey[sigindex1 - 1],
-                                 (const uint8_t *)FLASH_META_SIG1,
-                                 hash)) {  // failure
-      return SIG_FAIL;
-    }
+    return SIG_FAIL;
   }
   if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[sigindex2 - 1],
                                (const uint8_t *)FLASH_META_SIG2,
                                hash)) {  // failure
-    if (0 != ecdsa_verify_digest(&secp256k1, pubkey[sigindex2 - 1],
-                                 (const uint8_t *)FLASH_META_SIG2,
-                                 hash)) {  // failure
-      return SIG_FAIL;
-    }
+    return SIG_FAIL;
   }
   if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[sigindex3 - 1],
                                (const uint8_t *)FLASH_META_SIG3,
                                hash)) {  // failture
-    if (0 != ecdsa_verify_digest(&secp256k1, pubkey[sigindex3 - 1],
-                                 (const uint8_t *)FLASH_META_SIG3,
-                                 hash)) {  // failture
-      return SIG_FAIL;
-    }
+    return SIG_FAIL;
   }
 
   return SIG_OK;
@@ -176,26 +156,17 @@ int signatures_new_ok(const image_header *hdr, uint8_t store_fingerprint[32]) {
   if (hdr->sigindex1 == hdr->sigindex3) return SIG_FAIL;  // duplicate use
   if (hdr->sigindex2 == hdr->sigindex3) return SIG_FAIL;  // duplicate use
 
-  if (0 != ecdsa_verify_digest(&secp256k1, pubkey[hdr->sigindex1 - 1],
+  if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[hdr->sigindex1 - 1],
                                hdr->sig1, hash)) {  // failure
-    if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[hdr->sigindex1 - 1],
-                                 hdr->sig1, hash)) {  // failure
-      return SIG_FAIL;
-    }
+    return SIG_FAIL;
   }
-  if (0 != ecdsa_verify_digest(&secp256k1, pubkey[hdr->sigindex2 - 1],
-                               hdr->sig2, hash)) {  // failure
-    if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[hdr->sigindex2 - 1],
-                                 hdr->sig2, hash)) {  // failure
-      return SIG_FAIL;
-    }
+  if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[hdr->sigindex1 - 1],
+                               hdr->sig1, hash)) {  // failure
+    return SIG_FAIL;
   }
-  if (0 != ecdsa_verify_digest(&secp256k1, pubkey[hdr->sigindex3 - 1],
-                               hdr->sig3, hash)) {  // failure
-    if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[hdr->sigindex3 - 1],
-                                 hdr->sig3, hash)) {  // failure
-      return SIG_FAIL;
-    }
+  if (0 != ecdsa_verify_digest(&secp256k1, pubkey_Bixin[hdr->sigindex1 - 1],
+                               hdr->sig1, hash)) {  // failure
+    return SIG_FAIL;
   }
 
   return SIG_OK;
