d0310d5dc5d59467db2c92173a38dd4d7a775083
dahuanzi <lihuanza@163.com>
2020-06-05 14:25:42 +0800

    update bootloader version to 1.8.5

--- legacy/bootloader/version.h
+++ legacy/bootloader/version.h
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 8
-#define VERSION_PATCH 4
+#define VERSION_PATCH 5
 
-#define VERSION_MAJOR_CHAR "\x01"
+#define VERSION_MAJOR_CHAR "\x00"
 #define VERSION_MINOR_CHAR "\x08"
-#define VERSION_PATCH_CHAR "\x04"
+#define VERSION_PATCH_CHAR "\x05"
