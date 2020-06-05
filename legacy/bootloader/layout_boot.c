fa9c2d64329b604a4b480da885fd5405b02721c0
danielsocials <daniel.socials@gmail.com>
2020-06-05 11:16:35 +0800

    change bixin to capital letter

--- legacy/bootloader/layout_boot.c
+++ legacy/bootloader/layout_boot.c
@@ -5,7 +5,7 @@ void layoutBootHome(void) {
   if (layoutNeedRefresh()) {
     oledClear();
     oledDrawBitmap(30, 20, &bmp_BiXin_logo32);
-    oledDrawStringCenter(85, 20, "BiXin", FONT_STANDARD);
+    oledDrawStringCenter(85, 20, "BIXIN", FONT_STANDARD);
     oledDrawStringCenter(85, 30, "Bootloader", FONT_STANDARD);
     oledDrawStringCenter(85, 40,
                          VERSTR(VERSION_MAJOR) "." VERSTR(
