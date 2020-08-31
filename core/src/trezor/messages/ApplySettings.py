eab0ca63ab74daf5b186c9e34dec5484939fb347
dahuanzi <lihuanza@163.com>
2020-08-31 15:26:30 +0800

    legacy:modify AppllySettings proto

--- core/src/trezor/messages/ApplySettings.py
+++ core/src/trezor/messages/ApplySettings.py
@@ -23,6 +23,7 @@ class ApplySettings(p.MessageType):
         auto_lock_delay_ms: int = None,
         display_rotation: int = None,
         passphrase_always_on_device: bool = None,
+        safety_checks: EnumTypeSafetyCheckLevel = None,
         use_ble: bool = None,
         use_se: bool = None,
         is_bixinapp: bool = None,
@@ -30,7 +31,6 @@ class ApplySettings(p.MessageType):
         fastpay_confirm: bool = None,
         fastpay_money_limit: int = None,
         fastpay_times: int = None,
-        safety_checks: EnumTypeSafetyCheckLevel = None,
     ) -> None:
         self.language = language
         self.label = label
@@ -39,6 +39,7 @@ class ApplySettings(p.MessageType):
         self.auto_lock_delay_ms = auto_lock_delay_ms
         self.display_rotation = display_rotation
         self.passphrase_always_on_device = passphrase_always_on_device
+        self.safety_checks = safety_checks
         self.use_ble = use_ble
         self.use_se = use_se
         self.is_bixinapp = is_bixinapp
@@ -46,7 +47,6 @@ class ApplySettings(p.MessageType):
         self.fastpay_confirm = fastpay_confirm
         self.fastpay_money_limit = fastpay_money_limit
         self.fastpay_times = fastpay_times
-        self.safety_checks = safety_checks
 
     @classmethod
     def get_fields(cls) -> Dict:
@@ -58,12 +58,12 @@ class ApplySettings(p.MessageType):
             6: ('auto_lock_delay_ms', p.UVarintType, 0),
             7: ('display_rotation', p.UVarintType, 0),
             8: ('passphrase_always_on_device', p.BoolType, 0),
-            9: ('use_ble', p.BoolType, 0),
-            10: ('use_se', p.BoolType, 0),
-            11: ('is_bixinapp', p.BoolType, 0),
-            12: ('fastpay_pin', p.BoolType, 0),
-            13: ('fastpay_confirm', p.BoolType, 0),
-            14: ('fastpay_money_limit', p.UVarintType, 0),
-            15: ('fastpay_times', p.UVarintType, 0),
-            16: ('safety_checks', p.EnumType("SafetyCheckLevel", (0, 1)), 0),
+            9: ('safety_checks', p.EnumType("SafetyCheckLevel", (0, 1)), 0),
+            100: ('use_ble', p.BoolType, 0),
+            101: ('use_se', p.BoolType, 0),
+            102: ('is_bixinapp', p.BoolType, 0),
+            103: ('fastpay_pin', p.BoolType, 0),
+            104: ('fastpay_confirm', p.BoolType, 0),
+            105: ('fastpay_money_limit', p.UVarintType, 0),
+            106: ('fastpay_times', p.UVarintType, 0),
         }
