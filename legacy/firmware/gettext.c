3aebe9e932a7af2d358d9a64a93fddd7cc9272a1
dahuanzi <lihuanza@163.com>
2020-11-26 18:05:36 +0800

    legacy:correct c format

--- legacy/firmware/gettext.c
+++ legacy/firmware/gettext.c
@@ -7,21 +7,6 @@
 
 extern uint8_t ui_language;
 
-// static int stricmp(const char* s1, const char* s2) {
-//   char c1, c2;
-//   do {
-//     c1 = *s1++;
-//     c2 = *s2++;
-//     if (c1 >= 'A' && c1 <= 'Z') {
-//       c1 += 0x20;
-//     }
-//     if (c2 >= 'A' && c2 <= 'Z') {
-//       c2 += 0x20;
-//     }
-//   } while (c1 && (c1 == c2));
-//   return (c1 - c2);
-// }
-
 char* gettext(const char* msgid) {
   if (ui_language == 0) return (char*)msgid;
 
