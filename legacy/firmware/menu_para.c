d87f951bdbca8aeb97f528e3f6f9a0d25c0f3cc8
dahuanzi <lihuanza@163.com>
2021-01-06 13:25:26 +0800

    legacy:fix sleep parameter bug

--- legacy/firmware/menu_para.c
+++ legacy/firmware/menu_para.c
@@ -62,6 +62,6 @@ void menu_para_set_shutdown(int index) {
 }
 
 void menu_para_set_sleep(int index) {
-  uint32_t ms[4] = {60 * 1000, 2 * 60 * 1000, 5 * 60 * 1000, 10 * 60 * 100};
+  uint32_t ms[4] = {60 * 1000, 2 * 60 * 1000, 5 * 60 * 1000, 10 * 60 * 1000};
   config_setSleepDelayMs(ms[index]);
 }
