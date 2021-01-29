a9c2abcd7f34a3130d64cf084aaa411b67331bfa
dahuanzi <lihuanza@163.com>
2021-01-29 14:51:45 +0800

    legacy:fix bootloader version

--- legacy/bootloader/version.h
+++ legacy/bootloader/version.h
@@ -2,6 +2,6 @@
 #define VERSION_MINOR 8
 #define VERSION_PATCH 6
 
-#define VERSION_MAJOR_CHAR "\x00"
+#define VERSION_MAJOR_CHAR "\x01"
 #define VERSION_MINOR_CHAR "\x08"
 #define VERSION_PATCH_CHAR "\x05"
