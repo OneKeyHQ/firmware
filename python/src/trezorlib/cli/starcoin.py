2464d60286913cce11b8b11506815b5c111d89be
dydysy <200615@gmail.com>
2021-09-22 12:13:08 +0800

    stc: replace the license

--- python/src/trezorlib/cli/starcoin.py
+++ python/src/trezorlib/cli/starcoin.py
@@ -1,18 +1,20 @@
-# This file is part of the Trezor project.
-#
-# Copyright (C) 2012-2019 SatoshiLabs and contributors
-#
+# This file is part of the OneKey project, https://onekey.so/
+# 
+# Copyright (C) 2021 OneKey Team <core@onekey.so>
+# 
 # This library is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License version 3
-# as published by the Free Software Foundation.
-#
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the License along with this library.
-# If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import click
