90f95f7f0fae709dd3785ae26e6cdbdf8ddeb3ca
dahuanzi <lihuanza@163.com>
2021-10-14 12:24:26 +0800

    mini:update version

--- legacy/bootloader/version.h
+++ legacy/bootloader/version.h
@@ -1,11 +1,11 @@
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 8
-#define VERSION_PATCH 9
+#define VERSION_MINOR 9
+#define VERSION_PATCH 0
 
 #define VERSION_MAJOR_CHAR "\x01"
-#define VERSION_MINOR_CHAR "\x08"
-#define VERSION_PATCH_CHAR "\x09"
+#define VERSION_MINOR_CHAR "\x09"
+#define VERSION_PATCH_CHAR "\x00"
 
 // Keep same as firmware/version.h define
-#define ONEKEY_VERSION "2.1.8"
+#define ONEKEY_VERSION "2.1.9"
 #define CHIP_INFO "STM32F205RG"
