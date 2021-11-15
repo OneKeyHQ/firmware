0983aee44583f70572ba129a51a8a40aadfe3c20
dahuanzi <lihuanza@163.com>
2021-11-15 11:48:50 +0800

    mini:bump bootlaoder and firmware version

--- legacy/bootloader/version.h
+++ legacy/bootloader/version.h
@@ -1,11 +1,11 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 9
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
 #define VERSION_MAJOR_CHAR "\x01"
 #define VERSION_MINOR_CHAR "\x09"
-#define VERSION_PATCH_CHAR "\x00"
+#define VERSION_PATCH_CHAR "\x01"
 
 // Keep same as firmware/version.h define
-#define ONEKEY_VERSION "2.1.9"
+#define ONEKEY_VERSION "2.1.10"
 #define CHIP_INFO "STM32F205RG"
