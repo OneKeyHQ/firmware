f998edb113086f5298dcb57bed6b05d8fd7affa0
dahuanzi <lihuanza@163.com>
2021-12-27 14:35:35 +0800

    mini:update bootloader version to 1.9.2

--- legacy/bootloader/version.h
+++ legacy/bootloader/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 9
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 #define VERSION_MAJOR_CHAR "\x01"
 #define VERSION_MINOR_CHAR "\x09"
-#define VERSION_PATCH_CHAR "\x01"
+#define VERSION_PATCH_CHAR "\x02"
 
 // Keep same as firmware/version.h define
-#define ONEKEY_VERSION "2.1.10"
+#define ONEKEY_VERSION "2.1.11"
