a693ef40fa18221566750de73750c511b63bcba9
dahuanzi <lihuanza@163.com>
2022-06-24 12:55:39 +0800

    feat(firmware):update status bar display

--- core/src/trezor/lvglui/__init__.py
+++ core/src/trezor/lvglui/__init__.py
@@ -78,49 +78,125 @@ class StatusBar:
         return cls._instance
 
     def __init__(self):
+        self.offset_x = 0
         self.ble = lv.img(lv.layer_top())
         self.ble.set_src("A:/res/Bluetooth.png")
-        self.ble.align(lv.ALIGN.TOP_RIGHT, -30, 5)
+        self.ble.align(lv.ALIGN.TOP_RIGHT, -32, 0)
         self.ble.add_flag(lv.obj.FLAG.HIDDEN)
         self.battery = lv.img(lv.layer_top())
-        self.battery.set_src("A:/res/Status=50%.png")
-        self.battery.align(lv.ALIGN.TOP_RIGHT, -5, 5)
+        self.battery.add_flag(lv.obj.FLAG.HIDDEN)
+        self.battery.set_src("A:/res/Status=100%.png")
+        self.battery.align(lv.ALIGN.TOP_RIGHT, -2, 0)
         self.usb = lv.img(lv.layer_top())
         self.usb.set_src("A:/res/usb.png")
         self.usb.add_flag(lv.obj.FLAG.HIDDEN)
-        self.usb.align(lv.ALIGN.TOP_RIGHT, -55, 5)
+        self.usb.align(lv.ALIGN.TOP_RIGHT, -56, 0)
+        self.charging = lv.img(lv.layer_top())
+        self.charging.set_src("A:/res/Status=charging.png")
+        self.charging.add_flag(lv.obj.FLAG.HIDDEN)
+        self.charging.align(lv.ALIGN.TOP_RIGHT, 0, 0)
 
-    def show_ble(self, show: bool = False):
-        if show:
-            if self.ble.has_flag(lv.obj.FLAG.HIDDEN):
-                self.ble.clear_flag(lv.obj.FLAG.HIDDEN)
+        from .scrs import font_STATUS_BAR
+
+        self.battery_cap = lv.label(lv.layer_top())
+        self.battery_cap.set_style_text_font(
+            font_STATUS_BAR, lv.PART.MAIN | lv.STATE.DEFAULT
+        )
+        self.battery_cap.add_flag(lv.obj.FLAG.HIDDEN)
+        self.battery_cap.align(lv.ALIGN.TOP_RIGHT, -56, 8)
+        self.battery_cap.set_style_text_color(
+            lv.color_hex(0xFFFFFF), lv.PART.MAIN | lv.STATE.DEFAULT
+        )
+
+    def show_ble(self, status: int):
+        if self.ble.has_flag(lv.obj.FLAG.HIDDEN):
+            self.ble.clear_flag(lv.obj.FLAG.HIDDEN)
+        if status == 1:
+            self.ble.set_src("A:/res/Bluetooth_connected.png")
+        elif status in (2, 3):
+            self.ble.set_src("A:/res/Bluetooth.png")
+        elif status == 4:
+            self.ble.set_src("A:/res/Bluetooth_closed.png")
+
+        if self.charging.has_flag(lv.obj.FLAG.HIDDEN):
+            self.ble.align(lv.ALIGN.TOP_RIGHT, -32, 0)
         else:
-            if not self.ble.has_flag(lv.obj.FLAG.HIDDEN):
-                self.ble.add_flag(lv.obj.FLAG.HIDDEN)
+            self.ble.align(lv.ALIGN.TOP_RIGHT, -56 - self.offset_x, 0)
 
     def show_usb(self, show: bool = False):
         if show:
             if self.usb.has_flag(lv.obj.FLAG.HIDDEN):
                 self.usb.clear_flag(lv.obj.FLAG.HIDDEN)
+                if self.charging.has_flag(lv.obj.FLAG.HIDDEN):
+                    self.usb.align(lv.ALIGN.TOP_RIGHT, -56, 0)
+                else:
+                    self.usb.align(lv.ALIGN.TOP_RIGHT, -80 - self.offset_x, 0)
         else:
             if not self.usb.has_flag(lv.obj.FLAG.HIDDEN):
                 self.usb.add_flag(lv.obj.FLAG.HIDDEN)
 
     def set_battery_img(self, value: int):
-        icon_path = retrieve_icon_path(value)
+        if self.battery.has_flag(lv.obj.FLAG.HIDDEN):
+                self.battery.clear_flag(lv.obj.FLAG.HIDDEN)
+        if self.charging.has_flag(lv.obj.FLAG.HIDDEN):
+            icon_path = retrieve_icon_path(False, value)
+            self.battery.align(lv.ALIGN.TOP_RIGHT, -2, 0)
+            self.battery_cap.add_flag(lv.obj.FLAG.HIDDEN)
+            self.offset_x = 0
+        else:
+            icon_path = retrieve_icon_path(True, value)
+            self.battery.align(lv.ALIGN.TOP_RIGHT, -24, 0)
+            self.battery_cap.clear_flag(lv.obj.FLAG.HIDDEN)
+            self.battery_cap.set_text(str(value) + "%")
+            pos = lv.point_t()
+            self.battery_cap.get_letter_pos(len(str(value) + "%"), pos)
+            self.offset_x = pos.x
+            if not self.ble.has_flag(lv.obj.FLAG.HIDDEN):
+                self.ble.align(lv.ALIGN.TOP_RIGHT, -56 - self.offset_x, 0)
+            if not self.usb.has_flag(lv.obj.FLAG.HIDDEN):
+                self.usb.align(lv.ALIGN.TOP_RIGHT, -80 - self.offset_x, 0)
         self.battery.set_src(icon_path)
 
+    def show_charging(self, show: bool = False):
+        if show:
+            if self.charging.has_flag(lv.obj.FLAG.HIDDEN):
+                self.charging.clear_flag(lv.obj.FLAG.HIDDEN)
 
-def retrieve_icon_path(value: int) -> str:
-    if value == 101:
-        return "A:/res/Status=charging.png"
-    elif value >= 85:
-        return "A:/res/Status=100%.png"
-    elif value >= 75:
-        return "A:/res/Status=75%.png"
-    elif value >= 50:
-        return "A:/res/Status=50%.png"
-    elif value >= 25:
-        return "A:/res/Status=25%.png"
+        else:
+            if not self.charging.has_flag(lv.obj.FLAG.HIDDEN):
+                self.charging.add_flag(lv.obj.FLAG.HIDDEN)
+            if not self.ble.has_flag(lv.obj.FLAG.HIDDEN):
+                self.ble.align(lv.ALIGN.TOP_RIGHT, -32, 0)
+            if not self.usb.has_flag(lv.obj.FLAG.HIDDEN):
+                self.usb.align(lv.ALIGN.TOP_RIGHT, -56, 0)
+            self.battery.align(lv.ALIGN.TOP_RIGHT, -2, 0)
+
+
+def retrieve_icon_path(charging: bool, value: int) -> str:
+
+    if charging:
+        if value >= 80:
+            return "A:/res/Charging=100%.png"
+        elif value >= 60:
+            return "A:/res/Charging=80%.png"
+        elif value >= 40:
+            return "A:/res/Charging=60%.png"
+        elif value >= 20:
+            return "A:/res/Charging=40%.png"
+        elif value >= 5:
+            return "A:/res/Charging=20%.png"
+        else:
+            return "A:/res/Charging=5%.png"
     else:
-        return "A:/res/Status=0%.png"
+        if value >= 80:
+            return "A:/res/Status=100%.png"
+        elif value >= 60:
+            return "A:/res/Status=80%.png"
+        elif value >= 40:
+            return "A:/res/Status=60%.png"
+        elif value >= 20:
+            return "A:/res/Status=40%.png"
+        elif value >= 5:
+            return "A:/res/Status=20%.png"
+        else:
+            return "A:/res/Status=5%.png"
