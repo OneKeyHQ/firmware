04c7b99e53b8de1f6bd406af0a229976a69dedfe
Dan <pengfei.lulu@gmail.com>
2022-07-25 11:12:00 +0800

    fix(legacy): fix the label to show Onekey Classic by default

--- legacy/firmware/config.c
+++ legacy/firmware/config.c
@@ -798,7 +798,15 @@ bool config_getRootNode(HDNode *node, const char *curve) {
 }
 
 bool config_getLabel(char *dest, uint16_t dest_size) {
-  return sectrue == config_get_string(KEY_LABEL, dest, dest_size);
+  if (secfalse == config_get_string(KEY_LABEL, dest, dest_size)) {
+    memcpy(dest, "OneKey Classic", 15 /*strlen("OneKey Classic") + 1*/);
+  } else {
+    int len = strlen(dest);
+    if (0 == len) {
+      memcpy(dest, "OneKey Classic", 15);
+    }
+  }
+  return true;
 }
 
 bool config_getLanguage(char *dest, uint16_t dest_size) {
