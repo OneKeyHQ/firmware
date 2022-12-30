2867f6c3b2726fad573a77ea7d9b1ed8c4b3fd21
lulu <pengfei.lulu@gmail.com>
2022-12-30 10:42:22 +0800

    fix(core): fix OK-15914 & OK-15944 (#281)

--- core/src/apps/sui/helper.py
+++ core/src/apps/sui/helper.py
@@ -2,7 +2,9 @@ from ubinascii import hexlify
 
 from trezor.crypto.hashlib import sha3_256
 
-TRANSACTION_PREFIX = b"TransactionData::"
+# This is currently hardcoded with [IntentScope::TransactionData = 0, Version::V0 = 0, AppId::Sui = 0]
+# https://github.com/MystenLabs/sui/pull/6445
+INTENT_BYTES = b"\x00\x00\x00"
 
 
 def sui_address_from_pubkey(pub_key_bytes: bytes) -> str:
