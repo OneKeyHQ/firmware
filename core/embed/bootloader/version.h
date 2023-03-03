052731cfd0449f6312ba9f7124e67d493054848a
loatheb <zhangzhao@onekey.so>
2023-03-03 21:50:39 +0800

    fix(boot): hotfix bootloader to v2.3.7

--- core/embed/bootloader/version.h
+++ core/embed/bootloader/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 3
-#define VERSION_PATCH 6
+#define VERSION_PATCH 7
 #define VERSION_BUILD 0
 #define VERSION_UINT32                                            \
   (VERSION_MAJOR | (VERSION_MINOR << 8) | (VERSION_PATCH << 16) | \
@@ -8,7 +8,7 @@
 
 #define FIX_VERSION_MAJOR 2
 #define FIX_VERSION_MINOR 3
-#define FIX_VERSION_PATCH 6
+#define FIX_VERSION_PATCH 7
 #define FIX_VERSION_BUILD 0
 
 #define VERSION_MONOTONIC 1
