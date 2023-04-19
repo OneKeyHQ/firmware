c595c66ca7107ecb58972fe4dd9dcec43225a75d
Ritchie <ritchie@onekey.so>
2023-04-19 16:26:49 +0800

    feat(legacy): add kaspa support

---
+++ legacy/firmware/kaspa.h
@@ -0,0 +1,18 @@
+
+#ifndef __KASPA_H__
+#define __KASPA_H__
+#include <stdint.h>
+#include "bip32.h"
+#include "messages-kaspa.pb.h"
+
+void kaspa_get_address(uint8_t *pubkey, const uint8_t pubkey_len,
+                       const char *prefix, char *addr);
+void kaspa_sign_sighash(HDNode *node, const uint8_t *raw_message,
+                        uint32_t raw_message_len, uint8_t *signature,
+                        pb_size_t *signature_len);
+void kaspa_signing_init(const KaspaSignTx *msg);
+void kaspa_signing_abort(void);
+
+extern u_int16_t input_count;
+extern u_int16_t input_index;
+#endif  // __KASPA_H__
