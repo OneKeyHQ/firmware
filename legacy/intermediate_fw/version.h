92ecbd825e0f44f2cd593a38c1ed8fc9ea274152
lihuanhuan <lihuanhuan@onekey.so>
2023-05-06 09:40:33 +0800

    feat(legacy):update bootloader public key

--- legacy/intermediate_fw/version.h
+++ legacy/intermediate_fw/version.h
@@ -1,10 +1,10 @@
 // Matches the bootloader version included in this firmware
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 12
-#define VERSION_PATCH 1
+#define VERSION_MINOR 99
+#define VERSION_PATCH 99
 
 #define FIX_VERSION_MAJOR 1
-#define FIX_VERSION_MINOR 12
-#define FIX_VERSION_PATCH 0
+#define FIX_VERSION_MINOR 99
+#define FIX_VERSION_PATCH 99
 
 #define ONEKEY_VERSION_HEX 0x2B00
