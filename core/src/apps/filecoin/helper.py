8e6a61a0eab22dc9ec3761e21f8cf2f2f4b2bb9a
Dan <pengfei.lulu@gmail.com>
2023-05-17 16:49:00 +0800

    fix(core): fix filecoin address decode

--- core/src/apps/filecoin/helper.py
+++ core/src/apps/filecoin/helper.py
@@ -1,3 +1,4 @@
+from trezor import wire
 from trezor.crypto import base32
 from trezor.crypto.hashlib import blake2b
 
@@ -14,9 +15,30 @@ def pubkey_to_address(pubkey: bytes, network_prefix: str):
     return addr_str.lower().rstrip("=")
 
 
+def decompressLEB128(b: bytearray) -> int:
+    """Decode the unsigned leb128 encoded bytearray"""
+    r = 0
+    for i, e in enumerate(b):
+        r = r + ((e & 0x7F) << (i * 7))
+        if (e & 0x80) == 0:
+            return r
+    return r
+
+
 def format_protocol(addr: bytes, network_prefix: str):
+    protocol = addr[0]
     buf = addr
-    check_payload = buf[1:] + blake2b(data=buf, outlen=4).digest()
-    b = base32.encode(check_payload)
-    addr_str = network_prefix + "1" + b
+    if protocol == 1:  # ADDRESS_PROTOCOL_SECP256K1
+        check_payload = buf[1:] + blake2b(data=buf, outlen=4).digest()
+        b = base32.encode(check_payload)
+        addr_str = network_prefix + "1" + b
+    elif protocol == 4:  # ADDRESS_PROTOCOL_DELEGATED
+        check_payload = buf[2:] + blake2b(data=buf, outlen=4).digest()
+        b = base32.encode(check_payload)
+        id = decompressLEB128(bytearray(addr[1:]))
+        id_str = f"{id}f"
+        addr_str = network_prefix + "4" + id_str + b
+    else:
+        return wire.DataError("unexpected protocol")
+
     return addr_str.lower().rstrip("=")
