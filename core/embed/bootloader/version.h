64f6dcb3d4b0dfcb16464d064278af48616c9409
loatheb <zhangzhao@onekey.so>
2023-11-06 09:43:51 +0800

    chore: update boot v2.5.0 version

--- core/embed/bootloader/version.h
+++ core/embed/bootloader/version.h
@@ -1,14 +1,14 @@
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 4
-#define VERSION_PATCH 9
+#define VERSION_MINOR 5
+#define VERSION_PATCH 0
 #define VERSION_BUILD 0
 #define VERSION_UINT32                                            \
   (VERSION_MAJOR | (VERSION_MINOR << 8) | (VERSION_PATCH << 16) | \
    (VERSION_BUILD << 24))
 
 #define FIX_VERSION_MAJOR 2
-#define FIX_VERSION_MINOR 4
-#define FIX_VERSION_PATCH 9
+#define FIX_VERSION_MINOR 5
+#define FIX_VERSION_PATCH 0
 #define FIX_VERSION_BUILD 0
 
 #define VERSION_MONOTONIC 1
