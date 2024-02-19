7fcc0aceed06b83235a312e7e97cfba0e8fa4702
Dan <pengfei.lulu@gmail.com>
2024-02-19 19:03:23 +0800

    fix(core): fix OK-25644

--- core/src/apps/polkadot/transaction.py
+++ core/src/apps/polkadot/transaction.py
@@ -52,7 +52,7 @@ class Transaction:
         rawtx: codec.base.ScaleBytes, callPrivIdx: int
     ) -> "Transaction":
         tx = TransactionUnknown(rawtx)
-        if callPrivIdx == 1287:
+        if callPrivIdx in (1287, 1280):
             desc = Transaction._readAccountIdLookupOfT_V15(rawtx, 0)
             obj = codec.types.Compact(rawtx)
             balance = obj.decode(check_remaining=False)
@@ -80,7 +80,7 @@ class Transaction:
         rawtx: codec.base.ScaleBytes, callPrivIdx: int
     ) -> "Transaction":
         tx = TransactionUnknown(rawtx)
-        if callPrivIdx == 1031:
+        if callPrivIdx in (1031, 1024):
             desc = Transaction._readAccountIdLookupOfT_V15(rawtx, 2)
             obj = codec.types.Compact(rawtx)
             balance = obj.decode(check_remaining=False)
@@ -136,7 +136,7 @@ class Transaction:
         rawtx: codec.base.ScaleBytes, callPrivIdx: int
     ) -> "Transaction":
         tx = TransactionUnknown(rawtx)
-        if callPrivIdx == 7943:
+        if callPrivIdx in (7943, 7936):
             desc = Transaction._readAccountIdLookupOfT_V15(rawtx, 5)
             obj = codec.types.Compact(rawtx)
             balance = obj.decode(check_remaining=False)
@@ -215,7 +215,6 @@ class Transaction:
         moduleIdx = rawtx.get_next_bytes(1)
         idx = rawtx.get_next_bytes(1)
         callPrivIdx = (moduleIdx[0] << 8) + idx[0]
-
         tx = Transaction()
         if network == "polkadot":
             tx = Transaction.deserialize_polkadot(rawtx, callPrivIdx)
@@ -266,6 +265,7 @@ class TransactionUnknown(Transaction):
         self, ctx: Context, sender: str, chain_name: str, symbol: str, decimal: int
     ) -> None:
         from trezor.ui.layouts.lvgl import confirm_blind_sign_common
+
         await confirm_blind_sign_common(ctx, sender, self.data.data)
 
 
