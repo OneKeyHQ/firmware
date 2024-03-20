0c9ea82ecee9a3f1489ff3ede1177bb6a46692dc
guowei0105 <155709160+guowei0105@users.noreply.github.com>
2024-03-20 20:19:47 +0800

    modify signature display method (#537)
    modify signature display method

--- core/src/apps/nervos/sign_tx.py
+++ core/src/apps/nervos/sign_tx.py
@@ -2,14 +2,16 @@ from typing import TYPE_CHECKING
 
 from trezor import wire
 from trezor.crypto.curve import secp256k1
+from trezor.lvglui.scrs import lv
 from trezor.messages import NervosSignedTx, NervosSignTx, NervosTxAck, NervosTxRequest
-from trezor.ui.layouts import confirm_final
+from trezor.ui.layouts import confirm_blind_sign_common, confirm_final
 
 from apps.common import paths
 from apps.common.keychain import auto_keychain
 from apps.nervos.get_address import generate_ckb_short_address
 from apps.nervos.hash import ckb_hash, ckb_hasher
 
+from . import ICON, PRIMARY_COLOR
 from .utils import bytes_to_hex_str, extend_uint64, hex_to_bytes_custom
 
 if TYPE_CHECKING:
@@ -26,15 +28,17 @@ async def sign_tx(
         raise ValueError(f"Invalid network: {msg.network}")
     await paths.validate_path(ctx, keychain, msg.address_n)
 
+    ctx.primary_color, ctx.icon_path = lv.color_hex(PRIMARY_COLOR), ICON
+    node = keychain.derive(msg.address_n)
+    address = generate_ckb_short_address(node, network=msg.network)
+    await confirm_blind_sign_common(ctx, address, bytes(msg.data_initial_chunk))
     if msg.data_length is not None and msg.data_length > 0:
         data_total = msg.data_length
         data = bytearray()
         data_left = data_total
         hasher = ckb_hasher()
         hasher.update(msg.data_initial_chunk)
-        print("lendata_left:" + str(data_left))
         data_left -= len(msg.data_initial_chunk)
-        print("len(msg.raw_message):" + str(len(msg.data_initial_chunk)))
         while data_left > 0:
             resp = await send_request_chunk(ctx, data_left)
             data_left -= len(resp.data_chunk)
@@ -43,11 +47,9 @@ async def sign_tx(
         hash_bytes = hasher.digest()
         hex_str = bytes_to_hex_str(hash_bytes)
         tx_hash = "0x" + hex_str
-        # await confirm_blind_sign_common(ctx, address, bytes(data))
     else:
         tx_hash = ckb_hash(msg.data_initial_chunk)
 
-    # tx_hash = ckb_hash(msg.raw_message)
     hasher = ckb_hasher()
     hasher.update(hex_to_bytes_custom(tx_hash))
     len_buffer = bytearray()
@@ -55,8 +57,6 @@ async def sign_tx(
     hasher.update(len_buffer)
     hasher.update(msg.witness_buffer)
     message_byte = hasher.digest()
-    node = keychain.derive(msg.address_n)
-    address = generate_ckb_short_address(node, network=msg.network)
     private_key = node.private_key()
     await confirm_final(ctx, "NERVOS")
     unmodified_signature = secp256k1.sign(private_key, message_byte, False)
